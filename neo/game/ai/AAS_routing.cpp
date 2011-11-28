albertz/iodoom3

commit 2fb3c9fbf59ff2374fd6728dcd0d7c13cbf29c0c

Avoiding "lose precision" compilator error.

neo/game/ai/AAS_routing.cpp
@@ -108,7 +108,7 @@ idAASLocal::CalculateAreaTravelTimes
 */
 void idAASLocal::CalculateAreaTravelTimes(void) {
 	int n, i, j, numReach, numRevReach, t, maxt;
-	byte *bytePtr;
+	unsigned short *bytePtr;
 	idReachability *reach, *rev_reach;
 
 	// get total memory for all area travel times
@@ -132,7 +132,7 @@ void idAASLocal::CalculateAreaTravelTimes(void) {
 	}
 
 	areaTravelTimes = (unsigned short *) Mem_Alloc( numAreaTravelTimes * sizeof( unsigned short ) );
-	bytePtr = (byte *) areaTravelTimes;
+	bytePtr = areaTravelTimes;
 
 	for ( n = 0; n < file->GetNumAreas(); n++ ) {
 
@@ -167,7 +167,7 @@ void idAASLocal::CalculateAreaTravelTimes(void) {
 		}
 	}
 
-	assert( ( (unsigned int) bytePtr - (unsigned int) areaTravelTimes ) <= numAreaTravelTimes * sizeof( unsigned short ) );
+	assert( ( bytePtr - areaTravelTimes ) <= numAreaTravelTimes * sizeof( unsigned short ) );
 }
 
 /*

commit 52ec776c0157ee82ae956ab552baacd6056d3ace

Fix errors and clumsiness introduced with commit 2fb3c9fb

neo/game/ai/AAS_routing.cpp
@@ -149,7 +149,7 @@ void idAASLocal::CalculateAreaTravelTimes(void) {
 			}
 			reach->number = i;
 			reach->disableCount = 0;
-			reach->areaTravelTimes = (unsigned short *) bytePtr;
+			reach->areaTravelTimes = bytePtr;
 			for ( j = 0, rev_reach = file->GetArea( n ).rev_reach; rev_reach; rev_reach = rev_reach->rev_next, j++ ) {
 				t = AreaTravelTime( n, reach->start, rev_reach->end );
 				reach->areaTravelTimes[j] = t;
@@ -157,7 +157,7 @@ void idAASLocal::CalculateAreaTravelTimes(void) {
 					maxt = t;
 				}
 			}
-			bytePtr += j * sizeof( unsigned short );
+			bytePtr += j;
 		}
 
 		// if this area is a portal
@@ -167,7 +167,7 @@ void idAASLocal::CalculateAreaTravelTimes(void) {
 		}
 	}
 
-	assert( ( (unsigned long) bytePtr - (unsigned long) areaTravelTimes ) <= numAreaTravelTimes * sizeof( unsigned short ) );
+	assert((bytePtr - areaTravelTimes) <= numAreaTravelTimes);
 }
 
 /*