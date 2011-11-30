c51585ce0c07ae6a28635e4e7b648e00376c8d2e
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-11-30 20:00:23 +0100

    Revert "Use system libogg/libvorbis/libvorbisfile"
    This reverts commit b4a73f2bd15d45f6d4f0c6b2b7305042a85aaec9.
    
    It make sense for systems that don't provide these libraries to have them in the repository.

--- neo/sound/snd_decoder.cpp
+++ neo/sound/snd_decoder.cpp
@@ -30,8 +30,8 @@ If you have questions concerning this license or the applicable additional terms
 #pragma hdrstop
 
 #include "snd_local.h"
-#include <vorbis/codec.h>
-#include <vorbis/vorbisfile.h>
+#include "OggVorbis/vorbis/codec.h"
+#include "OggVorbis/vorbis/vorbisfile.h"
 
 
 /*
@@ -230,8 +230,7 @@ int idWaveFile::ReadOGG( byte* pBuffer, int dwSizeToRead, int *pdwSizeRead ) {
 	OggVorbis_File *ov = (OggVorbis_File *) ogg;
 
 	do {
-		int stream;
-		int ret = ov_read( ov, bufferPtr, total >= 4096 ? 4096 : total, Swap_IsBigEndian(), 2, 1, &stream );
+		int ret = ov_read( ov, bufferPtr, total >= 4096 ? 4096 : total, Swap_IsBigEndian(), 2, 1, &ov->stream );
 		if ( ret == 0 ) {
 			break;
 		}
@@ -549,9 +548,8 @@ int idSampleDecoderLocal::DecodeOGG( idSoundSample *sample, int sampleOffset44k,
 	totalSamples = sampleCount;
 	readSamples = 0;
 	do {
-		int stream;
 		float **samples;
-		int ret = ov_read_float( &ogg, &samples, totalSamples / sample->objectInfo.nChannels, &stream );
+		int ret = ov_read_float( &ogg, &samples, totalSamples / sample->objectInfo.nChannels, &ogg.stream );
 		if ( ret == 0 ) {
 			failed = true;
 			break;
